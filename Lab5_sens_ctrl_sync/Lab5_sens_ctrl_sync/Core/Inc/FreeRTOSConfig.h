a6b55b6233dfdb1f1c23632cef45ec26001b6c7b
kuan0113 <kevin28885506@gmail.com>
2025-10-03 18:13:11 -0400

    update

--- Lab5_sens_ctrl_sync/Lab5_sens_ctrl_sync/Core/Inc/FreeRTOSConfig.h
+++ Lab5_sens_ctrl_sync/Lab5_sens_ctrl_sync/Core/Inc/FreeRTOSConfig.h
@@ -45,7 +45,7 @@
 
 /* USER CODE BEGIN Includes */
 /* Section where include file can be added */
-
+#define traceTASK_SWITCHED_IN()  traceTaskSwitch()
 
 /* USER CODE END Includes */
 
@@ -166,7 +166,7 @@ standard names. */
 #define USE_CUSTOM_SYSTICK_HANDLER_IMPLEMENTATION 0
 
 /* USER CODE BEGIN Defines */
-
+/* Section where parameter definitions can be added (for instance, to override default ones in FreeRTOS.h) */
 /* USER CODE END Defines */
 
 #endif /* FREERTOS_CONFIG_H */
